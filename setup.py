1441ed8c234bfa05509263c04f1f1aa10977e44d
Ola Suaifan <osuaifan93@gmail.com>
2025-06-07 18:34:50 -0400

    structured the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative_medical AI Project',
+    version= '0.0.0',
+    author= 'Ola Suaifan',
+    author_email= 'suaifanola@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
